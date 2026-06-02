62a10c892e7ece87bf55932b8ee0e4a0f22bb791
markz0r <mark@mwc.one>
2026-06-02 10:45:34 +1000

    working test?

--- run_local_preview.sh
+++ run_local_preview.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 IMAGE="${PREVIEW_IMAGE:-nginx:alpine}"
 
 exec docker run --rm \
-  --publish 127.0.0.1:8088:80 \
+  --publish 8088:80 \
   --volume "${SCRIPT_DIR}:/usr/share/nginx/html:ro" \
   "${IMAGE}"
